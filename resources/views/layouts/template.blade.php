ee2611724b5cad0e9ee607ed47075e278339363c
marencoalf <alfredomarenco@boletea.com>
2020-11-16 08:05:00 -0600

    Agregamos el escaner provisional para las mascotas perdidas

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
     <title>Placas de identificación inteligentes para perros&mdash; Tamalindos </title>
     <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500&display=swap"
