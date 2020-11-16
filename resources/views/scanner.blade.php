167e0156355add2b922d32a256dd8914e5301f63
marencoalf <alfredomarenco@boletea.com>
2020-11-15 18:29:35 -0600

    Cambios en el formulario de creacion y edicion

--- resources/views/scanner.blade.php
+++ resources/views/scanner.blade.php
@@ -27,12 +27,4 @@
             alert(e);
         });
     </script>
-    <div class="btn-group btn-group-toggle mb-5" data-toggle="buttons">
-        <label class="btn btn-primary active">
-            <input type="radio" name="options" value="1" autocomplete="off" checked> Front Camera
-        </label>
-        <label class="btn btn-secondary">
-            <input type="radio" name="options" value="2" autocomplete="off"> Back Camera
-        </label>
-    </div>
 @endsection
