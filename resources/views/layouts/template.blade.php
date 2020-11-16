99275d324f43e62b9fe8f7c3e3a9b88919a6f196
marencoalf <alfredomarenco@boletea.com>
2020-11-16 08:31:54 -0600

    Agregamos la plantilla base de bootstrap

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <script type="text/javascript" src="js/instascan.min.js"></script>
 </head>
 
 <body>
