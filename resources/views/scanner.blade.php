5ee8d59e598d3fd4cbfa927a2faaa55141d3021c
marencoalf <alfredomarenco@boletea.com>
2020-11-16 10:03:04 -0600

    Agregamos el escaner provisional para las mascotas perdidas

--- resources/views/scanner.blade.php
+++ resources/views/scanner.blade.php
@@ -1,25 +1,53 @@
 @extends('layouts.template')
 
 @section('section')
-    <video id="preview" style="width: 100%; height: 100%;" class="border" ></video>
+    <video id="preview" class="border" style="width:100%; height: 100%;"></video>
+    <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js" rel="nofollow">
+    </script>
     <script type="text/javascript">
-        let scanner = new Instascan.Scanner({
+        var scanner = new Instascan.Scanner({
             video: document.getElementById('preview'),
-            mirror: false,
-            scanPeriod: 5
+            scanPeriod: 5,
+            mirror: false
         });
         scanner.addListener('scan', function(content) {
-            console.log(content);
+            //alert(content);
+            window.location.href=content;
         });
         Instascan.Camera.getCameras().then(function(cameras) {
             if (cameras.length > 0) {
                 scanner.start(cameras[0]);
+                $('[name="options"]').on('change', function() {
+                    if ($(this).val() == 1) {
+                        if (cameras[0] != "") {
+                            scanner.start(cameras[0]);
+                        } else {
+                            alert('No Front camera found!');
+                        }
+                    } else if ($(this).val() == 2) {
+                        if (cameras[1] != "") {
+                            scanner.start(cameras[1]);
+                        } else {
+                            alert('No Back camera found!');
+                        }
+                    }
+                });
             } else {
                 console.error('No cameras found.');
+                alert('No cameras found.');
             }
         }).catch(function(e) {
             console.error(e);
+            alert(e);
         });
 
     </script>
+    <div class="btn-group btn-group-toggle mb-5" data-toggle="buttons">
+        <label class="btn btn-primary active">
+            <input type="radio" name="options" value="1" autocomplete="off" checked> Front Camera
+        </label>
+        <label class="btn btn-secondary">
+            <input type="radio" name="options" value="2" autocomplete="off"> Back Camera
+        </label>
+    </div>
 @endsection
