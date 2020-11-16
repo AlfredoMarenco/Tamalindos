2c9f17d41ad7a2c8b3cef577d1330739c264461d
marencoalf <alfredomarenco@boletea.com>
2020-11-15 18:28:28 -0600

    Cambios en el formulario de creacion y edicion

--- resources/views/scanner.blade.php
+++ resources/views/scanner.blade.php
@@ -17,22 +17,7 @@
         });
         Instascan.Camera.getCameras().then(function(cameras) {
             if (cameras.length > 0) {
-                scanner.start(cameras[0]);
-                $('[name="options"]').on('change', function() {
-                    if ($(this).val() == 1) {
-                        if (cameras[0] != "") {
-                            scanner.start(cameras[0]);
-                        } else {
-                            alert('No Front camera found!');
-                        }
-                    } else if ($(this).val() == 2) {
-                        if (cameras[1] != "") {
-                            scanner.start(cameras[1]);
-                        } else {
-                            alert('No Back camera found!');
-                        }
-                    }
-                });
+                scanner.start(cameras[1]);
             } else {
                 console.error('No cameras found.');
                 alert('No cameras found.');
@@ -41,7 +26,6 @@
             console.error(e);
             alert(e);
         });
-
     </script>
     <div class="btn-group btn-group-toggle mb-5" data-toggle="buttons">
         <label class="btn btn-primary active">
